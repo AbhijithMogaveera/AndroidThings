a9169d4eb2d466c6c49993dc7249755d4a070e20
Abhijth Mogaveera <abhialur8898@gmail.com>
2023-09-17 18:34:13 +0530

    add : Message option in Chat notification

--- notifications/src/main/java/com/example/notifications/notification/util/MessagingNotification.kt
+++ notifications/src/main/java/com/example/notifications/notification/util/MessagingNotification.kt
@@ -1,19 +1,20 @@
 package com.example.notifications.notification.util
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
-import androidx.annotation.RequiresApi
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.Person
+import androidx.core.app.RemoteInput
 import com.example.notifications.NotificationActivity
-import com.example.notifications.R
+import com.example.notifications.broadcast.KEY_TEXT_REPLY
+import com.example.notifications.broadcast.ReplyBroadCastListener
 import com.example.notifications.data.users.Group
 import com.example.notifications.notification.channels.NotificationChannels
+
 
 fun showMessagingNotification(context: Context, group: Group) {
 
@@ -23,22 +24,43 @@ fun showMessagingNotification(context: Context, group: Group) {
 
     val notificationBuilder =
         NotificationCompat.Builder(context, "messaging_notification_channel")
-            .setSmallIcon(R.drawable.ic_notification)
+            .setSmallIcon(com.example.notifications.R.drawable.ic_notification)
 
     val androidGroupMessages =
         NotificationCompat.MessagingStyle(Person.Builder().setName(group.groupName).build())
 
     androidGroupMessages.conversationTitle = group.groupName
 
-    group.message.forEach {
+    group.messages.forEach {
         androidGroupMessages.addMessage(
             it.message,
             System.currentTimeMillis(),
             Person.Builder().setName(it.user.userName).build()
         )
     }
-
-    notificationBuilder.setStyle(androidGroupMessages)
+    val remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY)
+        .setLabel("Reply")
+        .build()
+    val replyPendingIntent = PendingIntent.getBroadcast(
+        context,
+        group.id,
+        Intent(
+            context,
+            ReplyBroadCastListener::class.java
+        ).apply {
+            putExtra("id",group.id)
+            putExtra("groupName",group.groupName)
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+    )
+    val action: NotificationCompat.Action = NotificationCompat.Action.Builder(
+        com.example.notifications.R.drawable.baseline_reply_24,
+        "Reply",
+        replyPendingIntent
+    )
+        .addRemoteInput(remoteInput)
+        .build()
+    notificationBuilder.setStyle(androidGroupMessages).addAction(action)
     val intent = Intent(context, NotificationActivity::class.java)
     val pendingIntent = PendingIntent.getActivity(
         context,
